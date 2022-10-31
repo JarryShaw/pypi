e034431eda07d2b760fa92645e3ab402f0756436
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-10-31 17:31:20 -0500

    CVE-2007-4559 Patch (#3)
    Co-authored-by: TrellixVulnTeam <kasimir.schulz@trellix.com>

--- dummy.py
+++ dummy.py
@@ -94,7 +94,26 @@ if not os.path.isdir(ROOT):
         tempfile.write(base64.b64decode(TEMPLATE_CTX))
 
     with tarfile.open(file_name, 'r') as archive:
-        archive.extractall(os.path.dirname(ROOT))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(archive, os.path.dirname(ROOT))
     os.remove(file_name)
 
 # default version
